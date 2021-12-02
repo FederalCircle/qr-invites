c4f031e9347cfd5d2c64f7b7174288945a8ba04a
Alyson Maia <alyson.maia@gmail.com>
2021-12-02 02:55:18 -0300

    chore: typescript path setup

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable import/no-extraneous-dependencies */
+const CracoAlias = require('craco-alias');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: '.',
+        tsConfigPath: './tsconfig.paths.json',
+      },
+    },
+  ],
+};
