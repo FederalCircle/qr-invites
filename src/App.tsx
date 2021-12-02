fdf95074b426ba2860796bd0e585950470938342
Alyson Maia <alyson.maia@gmail.com>
2021-12-02 03:03:41 -0300

    chore: firebase database setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { child, onValue, push, ref } from 'firebase/database';
+import { database } from './config/firebase';
+
+interface Guest {
+  name: string;
+}
 
 const App = () => {
+  const [guests, setGuests] = useState<Record<string, Guest>>({});
+  const addData = async () => {
+    try {
+      const id = push(child(ref(database), 'guests'), {
+        name: 'John Doe',
+      }).key;
+      console.log('Document written with ID: ', id);
+    } catch (e) {
+      console.error('Error adding document: ', e);
+    }
+  };
+
+  useEffect(() => {
+    const guestsRef = child(ref(database), 'guests');
+    onValue(guestsRef, (snapshot) => {
+      const data: Record<string, Guest> = snapshot.val();
+      if (data) {
+        console.log('Guests Retrieved', data);
+        setGuests(data);
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={addData}>Add</button>
+        <div>
+          <ul>
+            {Object.keys(guests).map((guestId) => (
+              <li key={guestId}>{guests[guestId].name}</li>
+            ))}
+          </ul>
+        </div>
       </header>
     </div>
   );
---
+++ src/constants/firebase.ts
@@ -0,0 +1,6 @@
+export const FIREBASE_API_KEY = 'AIzaSyAgOhdgvD4V5LPYOxmoj3s1c0GbcWjbAS8';
+export const FIREBASE_AUTH_DOMAIN = 'qr-invites.firebaseapp.com';
+export const FIREBASE_PROJECT_ID = 'qr-invites';
+export const FIREBASE_STORAGE_BUCKET = 'qr-invites.appspot.com';
+export const FIREBASE_MESSAGING_SENDER_ID = '687742328448';
+export const FIREBASE_APP_ID = '1:687742328448:web:2de64b0cba17b1c53d1065';
