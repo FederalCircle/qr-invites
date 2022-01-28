283360a51d75be5d75535c1f007657be74e35ae2
Alyson Maia <alyson.maia@gmail.com>
2022-01-28 07:00:05 -0300

    feat: list page styles

--- src/pages/ListPage/ListPage.tsx
+++ src/pages/ListPage/ListPage.tsx
@@ -1,14 +1,31 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import debounce from 'lodash.debounce';
 
+import InputLabel from '@mui/material/InputLabel';
+import Button from '@mui/material/Button';
+import FormControl from '@mui/material/FormControl';
+import InputAdornment from '@mui/material/InputAdornment';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import CheckIcon from '@mui/icons-material/Check';
+
 import useGuests from '@/hooks/useGuests';
 import useGuestsActions from '@/hooks/useGuestsActions';
 import { Guest } from '@/types';
 import AppBar from '@/components/AppBar';
+import useCodeCheckin from '@/pages/ScanPage/useCodeCheckin';
 
 const ListPage = () => {
   const { guests, getGuestsByName } = useGuests();
   const { checkinGuest } = useGuestsActions();
+  const { confirmGuest } = useCodeCheckin();
 
   const [guestsList, setGuestsList] = useState<Guest[]>([]);
 
@@ -28,6 +45,12 @@ const ListPage = () => {
     [bareHandleInputChange],
   );
 
+  const handleCheckinClick = (guest: Guest) => {
+    if (confirmGuest(guest)) {
+      checkinGuest(guest.id);
+    }
+  };
+
   useEffect(() => {
     setGuestsList(guests);
   }, [guests]);
@@ -35,19 +58,41 @@ const ListPage = () => {
   return (
     <>
       <AppBar title="Convidados" />
-      <input type="text" onChange={handleInputChange} />
-      <ul>
+
+      <FormControl variant="outlined" fullWidth>
+        <InputLabel htmlFor="search">Nome do convidado</InputLabel>
+        <OutlinedInput
+          id="search"
+          type="text"
+          endAdornment={
+            <InputAdornment position="end">
+              <SearchIcon />
+            </InputAdornment>
+          }
+          label="Password"
+          onChange={handleInputChange}
+        />
+      </FormControl>
+
+      <List>
         {guestsList.map((guest) => (
-          <li key={guest.id}>
-            {guest.name}{' '}
-            {guest.hasCheckin ? (
-              'DONE'
-            ) : (
-              <button onClick={() => checkinGuest(guest.id)}>Check-in</button>
-            )}
-          </li>
+          <ListItem disablePadding key={guest.id}>
+            <ListItemButton>
+              <ListItemIcon>
+                <PersonIcon />
+              </ListItemIcon>
+              <ListItemText primary={guest.name} />
+              {guest.hasCheckin ? (
+                <CheckIcon color="success" />
+              ) : (
+                <Button onClick={() => handleCheckinClick(guest)}>
+                  Check-in
+                </Button>
+              )}
+            </ListItemButton>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 };
